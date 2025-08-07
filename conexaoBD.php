57e297cc152d76c2bbacc9e52135a068fe94a861
_Vanes_ <20233002419@estudantes.ifpr.edu.br>
2025-08-07 15:26:42 -0300

    Update conexaoBD.php

--- conexaoBD.php
+++ conexaoBD.php
@@ -3,7 +3,7 @@
     $hostBD   = "localhost"; //Define o local do servidor do BD
     $userBD   = "root"; //Define o usuário do BD (Padrão: root)
     $senhaBD  = "root"; //Define a senha do BD (Padrão: "")
-    $database = "inf3"; //Define qual base será realizada a conexão
+    $database = "monitoramento-idoso"; //Define qual base será realizada a conexão
 
     //Função do PHP para estabelecer conexão com BD
     $conn     = mysqli_connect($hostBD, $userBD, $senhaBD, $database);
