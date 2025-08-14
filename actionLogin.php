df9dff6c00468f731a0db21379b7b5cf1ae03bdb
_Vanes_ <20233002419@estudantes.ifpr.edu.br>
2025-08-14 14:18:35 -0300

    Update actionLogin.php

--- actionLogin.php
+++ actionLogin.php
@@ -3,8 +3,8 @@
     session_start(); //função para inciar uma sessão
     include("conexaoBD.php");
 
-    $emailUsuario = mysqli_real_escape_string($link, $_POST["emailUsuario"]);
-    $senhaUsuario = mysqli_real_escape_string($link, $_POST["senhaUsuario"]);
+    $emailUsuario = mysqli_real_escape_string($conn, $_POST["emailUsuario"]);
+    $senhaUsuario = mysqli_real_escape_string($conn, $_POST["senhaUsuario"]);
 
 
     $buscarLogin ="SELECT *
@@ -38,3 +38,4 @@
     }
 
 ?>
+
